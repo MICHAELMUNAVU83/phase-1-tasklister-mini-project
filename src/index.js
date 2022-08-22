44bc0d0ef6705150b0e8366598e88fb2ad67464c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-08-22 09:24:14 +0300

    Completed the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("submit-btn").addEventListener("click", (e) => {
+    e.preventDefault()
+    const inputfield = document.getElementById("new-task-description").value;
+    document.getElementById('tasks').innerHTML += `
+       <li>${inputfield}</li>
+
+ 
+ 
+ `
+    document.getElementById("new-task-description").value = ''
+
+  })
+
+
+
 });
