40743f087689c3c44e8c0544b2b3cba9f3de64f2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-08-23 10:34:17 +0300

    Added delete functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("tasks").addEventListener("click", (e)=>{
+    if(e.target.classList.contains("delete-btn")){
+      e.target.parentElement.remove()
 
-  document.getElementById("submit-btn").addEventListener("click", (e) => {
-    e.preventDefault()
-    const inputfield = document.getElementById("new-task-description").value;
-    document.getElementById('tasks').innerHTML += `
-       <li>${inputfield}</li>
-
- 
- 
- `
-    document.getElementById("new-task-description").value = ''
-
+    }
+   
   })
 
+  document.getElementById("submit-btn").addEventListener("click", (e) => {
+    e.preventDefault();
 
-
+    const inputfield = document.getElementById("new-task-description").value;
+    document.getElementById("tasks").innerHTML += `
+    <div>
+       <li>${inputfield}</li>
+       <button type="button" class="delete-btn">x </button>
+       </div>
+     `;
+    document.getElementById("new-task-description").value = "";
+  });
 });
